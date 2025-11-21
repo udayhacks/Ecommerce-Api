3676ece3aa3313e941783e65fca8ec27c22762ce
alone_coder <blackdevil190402@gmail.com>
2025-11-21 21:19:58 +0530

    two more api method pending

---
+++ E-comProject/src/test/java/com/udayHacks/E_comProject/EComProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udayHacks.E_comProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
